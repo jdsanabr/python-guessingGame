8b2fce1f511b56692e70a01b9085e347f6c6278b
jdsanabr <jonathandavidsanabria@gmail.com>
2024-01-15 09:59:52 -0800

    Added comments to py file

---
+++ guessingGame.py
@@ -0,0 +1,22 @@
+
+"""
+This program will prompt the user to provide the parameter for a number guessing game. In this case,
+it will be the upper limit. For example, the program will say, "Pick a number between 1 and 20,"
+20 being the parameter provided by the user.
+
+Though this is a simple program, the primary purpose of this is for me to become familiar with Python syntax.
+"""
+
+# program greets user
+
+
+# ask user for upper limit
+
+
+# say: pick a number between 1 and [upper limit]
+
+
+# loop until correct number is guessed
+
+
+# congratulate user and print how many guesses it took
